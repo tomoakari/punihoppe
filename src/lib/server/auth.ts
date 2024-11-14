0185a5509798d3b86612af3453e2ac41ac7ff792
tomoyuki matsumoto <matsumoto.tomoyuki@valuesccg.com>
2024-11-14 22:11:31 +0900

    GoogleAuth関係をちょっと修正

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,9 +1,8 @@
 import { google } from 'googleapis';
-import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
 
 const oauth2Client = new google.auth.OAuth2(
-    GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET,
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
     'http://localhost:5173/auth/callback'
 );
 
