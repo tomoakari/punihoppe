553d07d0ff17a6af5e3e75cfc40a29f3233f9ca5
tomoyuki matsumoto <matsumoto.tomoyuki@valuesccg.com>
2024-11-14 22:21:13 +0900

    GoogleAuth関係をちょっと修正

--- src/routes/mypage/+page.svelte
+++ src/routes/mypage/+page.svelte
@@ -1,12 +1,5 @@
 <script lang="ts">
 
-    import type { PageServerLoad } from './$types';
-    import { getServerSession } from '@auth/sveltekit';
-
-    export const load: PageServerLoad = async (event) => {
-        const session = await getServerSession(event);
-        return { session };
-    };
 </script>
 
 mypage
