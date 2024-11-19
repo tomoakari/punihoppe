b09a69543d384cc757ecde52180026d0082e505b
tomoyuki matsumoto <matsumoto.tomoyuki@valuesccg.com>
2024-11-19 13:09:13 +0900

    環境編集の取得テスト

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
     import { Button } from "$lib/components/ui/button";
+
+    let metaenv = import.meta.env.FIREBASE_CLIENT_ID
+
+    let processenv = process.env.FIREBASE_CLIENT_ID
+    
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <Button variant="outline">しゃどえすえぬ</Button>
+<p>
+    metaenv: {metaenv}
+</p>
+<p>
+    processenv: {processenv}
+</p>
+
