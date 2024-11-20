9a224ac8cce5082b73bd5db63df8b456bc963eb6
tomoyuki matsumoto <matsumoto.tomoyuki@valuesccg.com>
2024-11-20 14:01:24 +0900

    画面遷移アニメーションを修正

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,6 +14,8 @@
 {@render children()}
 
 <style>
+  /*
+
 @keyframes fade-in {
   from {
     opacity: 0;
@@ -44,4 +46,5 @@
     210ms cubic-bezier(0, 0, 0.2, 1) 90ms both fade-in,
     300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-from-right;
 }
+    */
 </style>
